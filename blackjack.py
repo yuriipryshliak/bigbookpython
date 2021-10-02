97efde71572d0b8185206aaf760a627e923b2532
yuriipryshliak <91629694+yuriipryshliak@users.noreply.github.com>
2021-10-02 08:19:03 +0300

    Add files via upload

---
+++ blackjack.py
@@ -0,0 +1,47 @@
+"""Blackjack, by Al Sweigart al@inventwithpython.com
+The classic card game also known as 21. (This version doesn't have
+ splitting or insurance.)
+ More info at: https://en.wikipedia.org/wili/Blackjack
+ View this code at https://nostarch.com/big-book-small-python-projects
+ Tags: large, game, card game"""
+
+import random, sys
+
+# Set up the constants:
+HEARTS   = chr(9829) # Character 9829 is '♥'.
+DIAMONDS = chr(9830) # Character 9830 is '♦'.
+SPADES   = chr(9824) # Character 9824 is '♠'.
+CLUBS    = chr(9827) # Character 9827 is '♣'.
+# (A list of chr codes is at https://inventwithpython.com/charactermap)
+BACKSIDE = 'backside'
+
+
+def main():
+    print('''Blackjack, by Al Sweigart al@inventwithpython.com
+    
+      Rules:
+        Try to get as close to 21 without going over.
+        Kings, Queens, and Jacks are worth 10 points.
+        Aces are worth 1 or 11 points.
+        Cards 2 through 10 are worth their face value.
+        (H) it to take another card.
+        (S) tand to stop taking cards.
+        On your first play, you can (D)ouble down to increase your bet
+        but must hit exactly one more time before standing.
+        In case of a tie, the bet is returned to the player.
+        The dealer stops hitting at 17.''')
+
+    money = 5000
+    while True: # Main game loop.
+        # Check if player has run out of money:
+        if money <= 0:
+            print("You're broke!")
+            print("Good thing you weren't playing the real money.")
+            print('Thanks for playing!')
+            sys.exit()
+
+        # Let the player enter their bet for this round:
+        print('Money:', money)
+        bet = getBet(money)
+
+        # Give the dealer and player two cards
